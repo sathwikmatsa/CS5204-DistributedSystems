0537409b542d1612753611dda2db7d6c996681d4
Sathwik Matsa <sathwikmatsa@gmail.com>
2019-10-07 12:17:37 +0530

    implement custom mpi barrier using send, recv

---
+++ assignments/03_MPI_Barrier_implementation/mpi_iittp_barrier.h
@@ -0,0 +1,32 @@
+#include <mpi.h>
+
+void mpi_iittp_barrier() {
+    int n_processes;
+    MPI_Comm_size(MPI_COMM_WORLD, &n_processes);
+
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    char flag;
+    const int BAR_WAIT = 9999;
+    const int BAR_FREE = 9898;
+
+    if (rank == 0) { // root node
+        if(n_processes > 1) MPI_Recv(&flag, 1, MPI_BYTE, rank+1, BAR_WAIT, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if(n_processes > 2) MPI_Recv(&flag, 1, MPI_BYTE, rank+2, BAR_WAIT, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if(n_processes > 1) MPI_Send(&flag, 1, MPI_BYTE, rank+1, BAR_FREE, MPI_COMM_WORLD);
+        if(n_processes > 2) MPI_Send(&flag, 1, MPI_BYTE, rank+2, BAR_FREE, MPI_COMM_WORLD);
+    } else if ((rank+1)*2 <= n_processes) { // internal node
+        MPI_Recv(&flag, 1, MPI_BYTE, (rank+1)*2 - 1, BAR_WAIT, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if((rank+1)*2 + 1 <= n_processes) MPI_Recv(&flag, 1, MPI_BYTE, (rank+1)*2, BAR_WAIT, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        MPI_Send(&flag, 1, MPI_BYTE, (rank+1)/2 - 1, BAR_WAIT, MPI_COMM_WORLD);
+        MPI_Recv(&flag, 1, MPI_BYTE, (rank+1)/2 - 1, BAR_FREE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        MPI_Send(&flag, 1, MPI_BYTE, (rank+1)*2 - 1, BAR_FREE, MPI_COMM_WORLD);
+        if((rank+1)*2 + 1 <= n_processes) MPI_Send(&flag, 1, MPI_BYTE, (rank+1)*2, BAR_FREE, MPI_COMM_WORLD);
+    } else { // leaf node
+        MPI_Send(&flag, 1, MPI_BYTE, (rank+1)/2 - 1, BAR_WAIT, MPI_COMM_WORLD);
+        MPI_Recv(&flag, 1, MPI_BYTE, (rank+1)/2 - 1, BAR_FREE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+}
